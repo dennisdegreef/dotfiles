3c962c3c911ad847e27b6576a606a2847d91b416
Dennis de Greef <github@link0.net>
2016-05-23 00:26:26 +0200

    Add redshift on new install process

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+DOTFILES=$(pwd);
+SYMLINKS=${DOTFILES}/symlinks.txt
+
+if [[ ! -f $SYMLINKS ]];
+then
+	echo "Please create from:to mapping in $SYMLINKS";
+	exit 1;
+fi
+
+for link in $(cat $SYMLINKS);
+do
+	from=$DOTFILES$(echo $link | cut -d':' -f 1);
+	to=$HOME/$(echo $link | cut -d':' -f 2);
+
+	if [[ ! -L $to ]];
+	then
+		if [[ -e $to ]];
+		then
+			echo "ERROR: Target $to already exists"
+		else
+			echo "Linked $from to $to"
+			ln -s $from $to;
+		fi
+	fi
+done
